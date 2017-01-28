f6c133eeee90a1fa5fd48a351e9ecca6b0f66984
Javier escobar <cybersettler@babylonone.com>
2017-01-28 17:15:48 +0100

    Added backend configuration file

---
+++ generators/app/templates/src/main/webapp/backend/config/_config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  persistence: {
+    catalog: [
+      {
+        // supported attributes include NeDB options object
+        // see https://github.com/louischatriot/nedb#creatingloading-a-database
+        collectionName: "demo"
+      }
+    ]
+  }
+};
